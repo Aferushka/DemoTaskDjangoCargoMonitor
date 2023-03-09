55188f7890265a09d8226a58aca70d540af6469c
Vladimir Vyalkov <myevska@gmail.com>
2023-03-09 21:17:20 +0300

    Select now remembers his choice

--- base/views.py
+++ base/views.py
@@ -13,6 +13,7 @@ class CargoView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['select_option_list'] = list(CargoVehicleType.objects.values_list('model_name', flat=True))
+        context['selected_option'] = self.request.GET.get('modelname', '')
         return context
 
 
